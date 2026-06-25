b95fb150a4b059104f94062642ffa63261fb41d0
Motosio <meihaisenben@gmail.com>
2026-06-25 13:31:43 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,27 +1,121 @@
-const area =
-document.getElementById("player-area");
+const enemyArea =
+document.getElementById(
+    "enemy-area"
+);
 
-playerCards.forEach(card => {
+const playerArea =
+document.getElementById(
+    "player-area"
+);
 
-    const div =
-    document.createElement("div");
+const turnButton =
+document.getElementById(
+    "turnButton"
+);
 
-    div.innerHTML = `
-        <h3>${card.name}</h3>
-        HP ${card.hp}<br>
-        ATK ${card.atk}<br>
-        DEF ${card.def}
+turnButton.onclick =
+battleTurn;
+
+function removeDead() {
+
+    for (
+        let i = enemyDeck.length - 1;
+        i >= 0;
+        i--
+    ) {
+
+        if (
+            enemyDeck[i].hp <= 0
+        ) {
+
+            enemyDeck.splice(
+                i,
+                1
+            );
+        }
+    }
+
+    for (
+        let i = playerDeck.length - 1;
+        i >= 0;
+        i--
+    ) {
+
+        if (
+            playerDeck[i].hp <= 0
+        ) {
+
+            playerDeck.splice(
+                i,
+                1
+            );
+        }
+    }
+}
+
+function createCard(
+    unit,
+    isEnemy
+) {
+
+    const card =
+    document.createElement(
+        "div"
+    );
+
+    card.className =
+        `card ${
+            isEnemy
+            ? "enemy"
+            : "player"
+        }`;
+
+    card.innerHTML = `
+        <h3>${unit.name}</h3>
+
+        HP:
+        ${unit.hp}
+        /
+        ${unit.maxHp}
+        <br>
+
+        ATK:
+        ${unit.atk}
+        <br>
+
+        DEF:
+        ${unit.def}
     `;
 
-    area.appendChild(div);
-});
-
-button.onclick = battleTurn;
-
-function battleTurn(){
-
-    enemyCards[0].hp -=
-    playerCards[0].atk;
-
-    render();
+    return card;
 }
+
+function render() {
+
+    enemyArea.innerHTML = "";
+    playerArea.innerHTML = "";
+
+    enemyDeck.forEach(
+        enemy => {
+
+        enemyArea.appendChild(
+            createCard(
+                enemy,
+                true
+            )
+        );
+    });
+
+    playerDeck.forEach(
+        player => {
+
+        playerArea.appendChild(
+            createCard(
+                player,
+                false
+            )
+        );
+    });
+}
+
+render();
