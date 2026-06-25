7a18d275a5497bc303beb19dbb650178dcb326c7
Motosio <meihaisenben@gmail.com>
2026-06-25 13:30:24 +0900

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,23 +1,29 @@
-const playerCards = [
+const playerDeck = [
+
 {
     name: "騎士",
+
     hp: 50,
+    maxHp: 50,
+
     atk: 7,
-    def: 5
+    def: 5,
+
+    rarity: 4,
+    element: "light"
 },
+
 {
     name: "スライム",
-    hp: 35,
-    atk: 6,
-    def: 2
-}
-];
 
-const enemyCards = [
-{
-    name: "ゴブリン",
-    hp: 30,
-    atk: 5,
-    def: 1
+    hp: 35,
+    maxHp: 35,
+
+    atk: 6,
+    def: 2,
+
+    rarity: 4,
+    element: "water"
 }
+
 ];
