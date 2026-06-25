19cef99498165be3be2702d2140cb00c2dec8e37
Motosio <meihaisenben@gmail.com>
2026-06-25 13:24:53 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
+const area =
+document.getElementById("player-area");
 
+playerCards.forEach(card => {
+
+    const div =
+    document.createElement("div");
+
+    div.innerHTML = `
+        <h3>${card.name}</h3>
+        HP ${card.hp}<br>
+        ATK ${card.atk}<br>
+        DEF ${card.def}
+    `;
+
+    area.appendChild(div);
+});
