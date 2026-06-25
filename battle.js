4e771596a442135cb418842f3b4e3566f9bf64f5
Motosio <meihaisenben@gmail.com>
2026-06-25 13:31:21 +0900

    Update battle.js

--- battle.js
+++ battle.js
@@ -1,1 +1,50 @@
+function attack(attacker, target) {
 
+    let damage =
+        attacker.atk -
+        target.def;
+
+    damage = Math.max(
+        damage,
+        1
+    );
+
+    target.hp -= damage;
+}
+
+function battleTurn() {
+
+    playerDeck.forEach(player => {
+
+        const target =
+            enemyDeck.find(
+                enemy => enemy.hp > 0
+            );
+
+        if (!target) return;
+
+        attack(
+            player,
+            target
+        );
+    });
+
+    enemyDeck.forEach(enemy => {
+
+        const target =
+            playerDeck.find(
+                player => player.hp > 0
+            );
+
+        if (!target) return;
+
+        attack(
+            enemy,
+            target
+        );
+    });
+
+    removeDead();
+
+    render();
+}
