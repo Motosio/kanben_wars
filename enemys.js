f8a5f190fb574904e8562b176aab04e088fa4d36
Motosio <meihaisenben@gmail.com>
2026-06-25 13:30:52 +0900

    Create enemys.js

---
+++ enemys.js
@@ -0,0 +1,27 @@
+const enemyDeck = [
+
+{
+    name: "勘弁ネズミ",
+
+    hp: 20,
+    maxHp: 20,
+
+    atk: 4,
+    def: 1,
+
+    element: "wood"
+},
+
+{
+    name: "勘弁コウモリ",
+
+    hp: 15,
+    maxHp: 15,
+
+    atk: 5,
+    def: 0,
+
+    element: "dark"
+}
+
+];
